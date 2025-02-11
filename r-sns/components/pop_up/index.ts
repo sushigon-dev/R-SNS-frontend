41d4e71f10914514960477ebbcb9f34006b6bac8
yonep <124860979+yonep3904@users.noreply.github.com>
2025-02-12 01:14:00 +0900

    Revert "Feature rootpage"

--- r-sns/components/pop_up/index.ts
+++
@@ -1,6 +0,0 @@
-import AlertDialog from "./AlertDialog/AlertDialog";
-import Dialog from "./Dialog/Dialog";
-import Drawer from "./Drawer/Drawer";
-import ResponsiveDialog from "./ResponsiveDialog/ResponsiveDialog";
-
-export { AlertDialog, Dialog, Drawer, ResponsiveDialog };
